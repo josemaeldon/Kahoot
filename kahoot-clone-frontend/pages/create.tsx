1fd139df7070cc912dc46a249d26b9208ac18d01
haotianGan <haotiangan@gmail.com>
2022-05-24 15:59:32 -0400

    flag

--- kahoot-clone-frontend/pages/create.tsx
+++ kahoot-clone-frontend/pages/create.tsx
@@ -3,15 +3,35 @@ import styles from "../styles/create.module.css";
 
 import Image from "next/image";
 
+interface KahootGame {
+  id: string;
+  author: string;
+  title: string;
+  questions: Question[];
+}
+
+interface Question {
+  question: string;
+  choices: string[];
+  correctAnswer: number;
+}
+
 function Header() {
   return (
     <div className={`${styles.container}`}>
-      <Image
-        src={"/kahootLogo.svg"}
-        width={"96px"}
-        height={"32.72px"}
-        alt="Kahoot Logo"
-      ></Image>
+      <div>
+        <Image
+          src={"/kahootLogo.svg"}
+          width={"96px"}
+          height={"32.72px"}
+          alt="Kahoot Logo"
+        ></Image>
+        <input type={"text"} placeholder="Enter Kahoot title..."></input>
+      </div>
+      <div>
+        <button type="button">Exit</button>
+        <button type="button">Save</button>
+      </div>
     </div>
   );
 }
