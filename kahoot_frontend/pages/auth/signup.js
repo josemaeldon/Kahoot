e46a8cfe79c662ed4ec07750f3592b78d5a90505
haotianGan <haotiangan@gmail.com>
2022-05-24 12:28:47 -0400

    seperated server and frontend files

---
+++ kahoot_frontend/pages/auth/signup.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Header from "../../Components/Header.js";
+import styles from "../../styles/signup.module.css";
+import Link from "next/link";
+function Signup() {
+  return (
+    <div className={`vh100`}>
+      <Header></Header>
+      <div className={`${styles.container}`}>
+        <div className={`${styles.card}`}>
+          <form>
+            <p>Log in</p>
+            <label for="username">Username or email</label>
+            <input type="text" id="username"></input>
+            <label for="password">Password</label>
+            <input type="password" id="password"></input>
+            <p>
+              Forgot Password?{" "}
+              <Link href="/auth/forgotpassword">
+                <a>Reset your password</a>
+              </Link>
+            </p>
+            <button type="submit">Log in</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
