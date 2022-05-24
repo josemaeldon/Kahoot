5a24247297ced2b890ef4804ad5ff94b0949fd19
rengzhengcodes <rzhengcontacts@gmail.com>
2022-05-24 08:34:32 -0400

    variable websocket sends and requests

--- pages/game/play.js
+++ pages/game/play.js
@@ -1,13 +1,19 @@
 // Create WebSocket connection.
 const socket = new WebSocket('wss://ws.postman-echo.com/raw');
 
-// Connection opened
-socket.addEventListener('open', function (event) {
-	socket.send('Hello Server');
-});
-
 //  Listen for messages
-socket.addEventListener('message',
-function(event) {
+var receiveMessage = (e) => {
 	console.log('Message from server: ', event.data);
-});
+}
+
+socket.addEventListener('message', receiveMessage);
+
+// Send messages
+var sendMessage = (e) => {
+	const message = document.getElementById("message").value;
+	console.log('Sending: ', message);
+	socket.send(message);
+}
+
+var sendButton = document.getElementById("send");
+sendButton.addEventListener("click", sendMessage);
