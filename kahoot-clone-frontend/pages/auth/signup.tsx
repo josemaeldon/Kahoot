2c339444f04285f5110c5f3bbe27bc9ca8fe918e
haotianGan <haotiangan@gmail.com>
2022-05-31 10:15:41 -0400

    wtf

--- kahoot-clone-frontend/pages/auth/signup.tsx
+++ kahoot-clone-frontend/pages/auth/signup.tsx
@@ -5,7 +5,22 @@ import { useState } from "react";
 import { postData } from "@lib/postData";
 import { APIResponse } from "pages/api/signup";
 import { useRouter } from "next/router";
-import Header from "@components/Header";
+
+import headerStyles from "@styles/Header.module.css";
+import Image from "next/image";
+
+function Header() {
+  return (
+    <div className={`${headerStyles.container}`}>
+      <Image
+        src={"/kahootLogo.svg"}
+        width={"96px"}
+        height={"32.72px"}
+        alt="Kahoot Logo"
+      ></Image>
+    </div>
+  );
+}
 
 interface Info {
   username: string;
