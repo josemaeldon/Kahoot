6be8efb1e72af6b8411a98e63b06fbb6ef568027
rengzhengcodes <rzhengcontacts@gmail.com>
2022-06-01 08:39:01 -0400

    names display on join

--- pages/host.js
+++ pages/host.js
@@ -4,17 +4,26 @@ const socket = new WebSocket("ws://64.225.12.53/ws");
 const createRmBtn = document.getElementById("createRmBtn");
 // rm id
 const display = document.getElementById("display");
+// name display
+const names = document.getElementById("names");
 //  Listen for messages
 var receiveMessage = (e) => {
 	console.log("Message from server: ", event.data);
 	let data = JSON.parse(event.data);
+	console.log(data);
 
 	switch (data.type) {
 		case "roomCreated":
 			display.innerText = `Room ID: ${data.roomId}`;
 			break;
 		case "userJoined":
-
+			let child = document.createElement("div");
+			child.classList.add("small-12");
+			child.classList.add("medium-4");
+			child.classList.add("large-2");
+			child.classList.add("nameDisplay");
+			child.innerText = data.username;
+			names.appendChild(child);
 			break;
 	}
 };
