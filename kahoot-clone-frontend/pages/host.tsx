033d2dce00d083fcc8f3826b6664c821ddbc9e36
José Maeldon da Silva <54424205+josemaeldon@users.noreply.github.com>
2024-12-02 00:26:00 -0300

    Update host.tsx

--- kahoot-clone-frontend/pages/host.tsx
+++ kahoot-clone-frontend/pages/host.tsx
@@ -43,11 +43,8 @@ function JoinHeader() {
   
   return (
     <div className={`${styles.darkBackground}`}>
-      <div className={`${styles.outerContainer}`}>
-        <div className={`${styles.playHeader}`}>
-          
-          <p>Acesse kahoot.cloudbr.app/play ou pelo QrCode</p>          
-        </div>
+      <div className={`${styles.outerContainer}`}>          
+          <p>Acesse kahoot.cloudbr.app/play ou pelo QrCode</p>
         <div className={`${styles.pinHeader}`}>
           <p>Game Pin:</p>
           <p>{`${r1} ${r2} ${r3}`}</p>
