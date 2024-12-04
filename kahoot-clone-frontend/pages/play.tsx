dcac8b193a8c5e47eaaaa285864df27f932bea34
José Maeldon da Silva <54424205+josemaeldon@users.noreply.github.com>
2024-12-04 12:16:30 -0300

    Update play.tsx

--- kahoot-clone-frontend/pages/play.tsx
+++ kahoot-clone-frontend/pages/play.tsx
@@ -121,42 +121,21 @@ function StartScreen() {
             readOnly={inputLocked}
           ></input>
           <button
-  className={`${styles.gameButton}`}
-  onClick={() => {
-    setInputLocked(true);
-  }}
->
-  {inputLocked && (
-    <span>
-      <Spinner
-        animation="border"
-        style={{ height: "24px", width: "24px" }}
-      ></Spinner>
-    </span>
-  )}
-  {!inputLocked && <span>Entrar</span>}
-</button>
-
-<button
-  className={`${styles.gameButton}`}
-  onClick={() => {
-    setInputLocked(true);
-    setTimeout(() => {
-      window.location.href = "https://kahoot.cloudbr.app";
-    }, 1000); // Adiciona um pequeno delay para simular o carregamento
-  }}
->
-  {inputLocked && (
-    <span>
-      <Spinner
-        animation="border"
-        style={{ height: "24px", width: "24px" }}
-      ></Spinner>
-    </span>
-  )}
-  {!inputLocked && <span>Sair</span>}
-</button>
-
+            className={`${styles.gameButton}`}
+            onClick={() => {
+              setInputLocked(true);
+            }}
+          >
+            {inputLocked && (
+              <span>
+                <Spinner
+                  animation="border"
+                  style={{ height: "24px", width: "24px" }}
+                ></Spinner>
+              </span>
+            )}
+            {!inputLocked && <span>Enter</span>}
+          </button>
         </div>
       </div>
     </div>
