d8affb12596cd4e274cf6264e25651850d594ac2
José Maeldon da Silva <54424205+josemaeldon@users.noreply.github.com>
2025-08-14 07:13:53 -0300

    Update mongoCache.ts

--- kahoot-clone-frontend/serverless/mongoCache.ts
+++ kahoot-clone-frontend/serverless/mongoCache.ts
@@ -7,7 +7,7 @@ export const connectToDatabase = async () => {
   }
 
   const uri =
-    "mongodb://root:Trino005%40@137.184.221.75:27017/kahoot?authSource=admin";
+    "mongodb://josemaeldon:b6096adea46c36bf140504a221808930@173.212.245.72:27017/kahoot?authSource=josemaeldon";
   const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
   await client.connect();
   cachedDb = client;
